f428e6d7976bd44f3f9e930f9f090b470095e669
lucas <lucas@computex.com.br>
2025-07-31 16:06:25 -0300

    Adicionando controle de estado na feature de profile

--- web/src/app/features/profile/pages/profile-client-page/profile-client-page.component.ts
+++ web/src/app/features/profile/pages/profile-client-page/profile-client-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, type OnInit } from '@angular/core';
+import { Component, computed, inject, signal, type OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialog } from '@angular/material/dialog';
@@ -12,9 +12,13 @@ import { Client } from '../../../clients/models/client.model';
 import { DialogUpdateClientInfoComponent, type DataDialogUpdateClientInfoInterface } from '../../components/dialog-update-client-info/dialog-update-client-info.component';
 import { DialogUpdateClientPasswordComponent, type DataDialogUpdateClientPasswordInterface } from '../../components/dialog-update-client-password/dialog-update-client-password.component';
 import { NgxMaskPipe } from 'ngx-mask';
+import { UiErrorComponent } from '../../../../shared/components/ui-error/ui-error.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-interface ProfileClientPageState {
-
+interface ProfileClientState {
+  client: Client | null;
+  loading: boolean;
+  error: string | null
 }
 
 @Component({
@@ -25,7 +29,9 @@ interface ProfileClientPageState {
     MatInputModule,
     MatFormFieldModule,
     CommonModule,
-    NgxMaskPipe
+    NgxMaskPipe,
+    UiErrorComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './profile-client-page.component.html',
 })
@@ -33,31 +39,52 @@ export class ProfileClientPageComponent implements OnInit {
   readonly dialog = inject(MatDialog);
   private authService = inject(AuthService);
   private clientService = inject(ClientService);
+
+  private state = signal<ProfileClientState>({
+    client: null, 
+    loading: true, // Inicia como true para a carga inicial
+    error: null,
+  });
   
-  clientId = toSignal(this.authService.currentUserId$);
-  clientRole = toSignal(this.authService.currentUserRole$);
-  public client!: Client; 
+  // Obtem o ID e o cargo do usuário autenticado
+  authUserId = toSignal(this.authService.currentUserId$);
+  authUserRole = toSignal(this.authService.currentUserRole$);
+  
+  public client = computed(() => this.state().client);
+  public isLoading = computed(() => this.state().loading);
+  public error = computed(() => this.state().error);
 
   ngOnInit(): void {
     this.loadInitialInfo();
   }
 
   loadInitialInfo(): void {
-    const clientId = this.clientId();
-    if(!clientId) return console.error(`ClienteId não encontrado`);
-    this.clientService.getClientById(clientId).subscribe({
+    this.state.update(s => ({ client: null, loading: true, error: null }));
+
+    const authUserId = this.authUserId();
+    if(!authUserId) {
+      console.error(`ID do usuário autenticado não encontrado`);
+      this.state.update(s => ({ ...s, error: "Ocorreu um erro, tente novamente mais tarde" }));
+      return;
+    }
+
+    this.clientService.getClientById(authUserId).subscribe({
       next: (data) => {
-        this.client = data;
+        this.state.update(s => ({ client: data, loading: false, error: null}));
       },
       error: (err) => {
         console.error(`Erro ao buscar informações do usuário: ${err.message}`);
+        this.state.update(s => ({ client: null, loading: false, error: "Ocorreu um erro ao buscar as informações do seu usuário, tente novamente mais tarde" }));
       }
     });
   }
 
   openDialogUpdateClientInfo() {
+    const client = this.client();
+    if(!client) return;
+
     const dialogData: DataDialogUpdateClientInfoInterface = {
-      client: this.client
+      client: client
     };
 
     const dialogRef = this.dialog.open(DialogUpdateClientInfoComponent, {
@@ -71,8 +98,11 @@ export class ProfileClientPageComponent implements OnInit {
   }
 
   openDialogUpdateClientPassword() {
+    const client = this.client();
+    if(!client) return;
+
     const dialogData: DataDialogUpdateClientPasswordInterface = {
-      clientId: this.client.id
+      clientId: client.id
     };
 
     this.dialog.open(DialogUpdateClientPasswordComponent, {
@@ -82,11 +112,11 @@ export class ProfileClientPageComponent implements OnInit {
   }
 
   getClientRole(): string {
-    const clientRole: AuthenticatedUserRole | undefined = this.clientRole();
+    const authUserRole: AuthenticatedUserRole | undefined = this.authUserRole();
 
-    if(clientRole === "COMPUTEX") return "COMPUTEX";
-    else if(clientRole === "CLIENT") return "Cliente";
-    else if(clientRole === "CLIENT_USER") return "Usuário";
+    if(authUserRole === "COMPUTEX") return "COMPUTEX";
+    else if(authUserRole === "CLIENT") return "Cliente";
+    else if(authUserRole === "CLIENT_USER") return "Usuário";
     else return "";
   }
 }
--- web/src/app/features/profile/pages/profile-user-page/profile-user-page.component.ts
+++ web/src/app/features/profile/pages/profile-user-page/profile-user-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, type OnInit } from '@angular/core';
+import { Component, computed, inject, signal, type OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AuthService } from '../../../../core/services/auth.service';
 import { toSignal } from '@angular/core/rxjs-interop';
@@ -11,6 +11,14 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
 import { DialogUpdateUserInfoComponent, type DataDialogUpdateClientUserInfoInterface } from '../../components/dialog-update-user-info/dialog-update-user-info.component';
 import { DialogUpdateUserPasswordComponent, type DataDialogUpdateClientUserPasswordInterface } from '../../components/dialog-update-user-password.component/dialog-update-user-password.component';
+import { UiErrorComponent } from '../../../../shared/components/ui-error/ui-error.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+interface ProfileUserState {
+  clientUser: ClientUserInterface | null;
+  loading: boolean;
+  error: string | null;
+}
 
 @Component({
   selector: 'app-profile-user-page',
@@ -19,7 +27,9 @@ import { DialogUpdateUserPasswordComponent, type DataDialogUpdateClientUserPassw
     MatButtonModule,
     MatInputModule,
     MatFormFieldModule,
-    CommonModule
+    CommonModule,
+    UiErrorComponent,
+    MatProgressSpinnerModule
   ],
   templateUrl: './profile-user-page.component.html',
 })
@@ -28,30 +38,49 @@ export class ProfileUserPageComponent implements OnInit {
   private authService = inject(AuthService);
   private clientUserService = inject(ClientUsersService);
   
-  clientUserId = toSignal(this.authService.currentUserId$);
-  clientUserRole = toSignal(this.authService.currentUserRole$);
-  public clientUser!: ClientUserInterface; 
+  authUserId = toSignal(this.authService.currentUserId$);
+
+  private state = signal<ProfileUserState>({
+    clientUser: null,
+    loading: true,
+    error: null
+  });
+
+  public clientUser = computed(() => this.state().clientUser);
+  public isLoading = computed(() => this.state().loading);
+  public error = computed(() => this.state().error);
 
   ngOnInit(): void {
     this.loadInitialInfo();
   }
 
   loadInitialInfo(): void {
-    const clientUserId = this.clientUserId();
-    if(!clientUserId) return console.error(`ClientUserId não encontrado`);
-    this.clientUserService.getClientUserById("999", clientUserId).subscribe({
+    this.state.set({ clientUser: null, loading: true, error: null });
+
+    const authUserId = this.authUserId();
+    if(!authUserId) {
+      console.error(`ID do usuário autenticado não encontrado`);
+      this.state.update(s => ({ ...s, error: "Ocorreu um erro, tente novamente mais tarde" }));
+      return;
+    }
+
+    this.clientUserService.getClientUserById("999", authUserId).subscribe({
       next: (data) => {
-        this.clientUser = data;
+        this.state.set({ clientUser: data, loading: false, error: null });
       },
       error: (err) => {
         console.error(`Erro ao buscar informações do usuário: ${err.message}`);
+        this.state.set({ clientUser: null, loading: false, error: "Ocorreu um erro ao buscar as informações do seu usuário, tente novamente mais tarde" });
       }
     });
   }
 
   openDialogUpdateClientInfo() {
+    const clientUser = this.clientUser();
+    if(!clientUser) return;
+
     const dialogData: DataDialogUpdateClientUserInfoInterface = {
-      clientUser: this.clientUser
+      clientUser: clientUser
     };
 
     const dialogRef = this.dialog.open(DialogUpdateUserInfoComponent, {
@@ -65,9 +94,12 @@ export class ProfileUserPageComponent implements OnInit {
   }
 
   openDialogUpdateClientPassword() {
+    const clientUser = this.clientUser();
+    if(!clientUser) return;
+
     const dialogData: DataDialogUpdateClientUserPasswordInterface = {
-      clientUserId: this.clientUser.id,
-      clientId: this.clientUser.clientId,
+      clientUserId: clientUser.id,
+      clientId: clientUser.clientId,
     };
 
     this.dialog.open(DialogUpdateUserPasswordComponent, {
